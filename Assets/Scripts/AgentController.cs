38ad77b0cb90a9ea8740d7b12b5f480c4c36cbba
Karol Kostyra <karol.m.kostyra@gmail.com>
2021-01-06 17:55:35 +0100

    Move agent by mouse click

---
+++ Assets/Scripts/AgentController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AgentController : MonoBehaviour
+{
+    [SerializeField] private GameObject[] wayPoints;
+    [SerializeField] private Camera cam;
+    [SerializeField] private NavMeshAgent agent;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+    }
+}
